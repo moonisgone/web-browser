55a85dadc548cdd094c70f6a67e570a70591799d
moonisgone <53594508+moonisgone@users.noreply.github.com>
2021-05-08 21:56:28 +0900

    upload

---
+++ game-refactoring/src/popup.js
@@ -0,0 +1,16 @@
+'use strict'
+
+class PopUp{
+    constructor(){
+        this.popUp = document.querySelector('.pop-up');
+        this.popUpText = document.querySelector('.pop-up_message');
+        this.popUpRefresh = document.querySelector('.pop-up_refresh');
+        popUpRefresh.addEventListener('click',()=>{
+
+        })
+
+        setClickListener(onClick){
+            this.onClick = onClick;
+        }
+    }
+}
